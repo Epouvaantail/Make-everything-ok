0f29aea9e6eaea0b69166bcf7d16ad6283498531
Epouvaantail <coutouzis.kilian@gmail.com>
2024-02-08 10:07:22 +0100

    add song

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ function modale() {
     const rembtn = document.querySelector(".btn")
     rembtn.remove();
     const body = document.querySelector("body");
-    body.insertAdjacentHTML("afterbegin", 
-    '<div class="modale"> <h1>Everything is OK now !</h1> <p>If everything is still not OK, try checking your settings of perception of objective reality and GO TO THE GYM.</p><button onClick={location.reload()} class="btn"><strong><span>LET&lsquo;S GO</span></strong></button> </div>'
+    body.insertAdjacentHTML("afterbegin",
+      '<div class="modale"> <h1>Everything is OK now !</h1> <p>If everything is still not OK, try checking your settings of perception of objective reality and GO TO THE GYM.</p><button onClick={location.reload()} class="btn"><strong><span>LET&lsquo;S GO</span></strong></button> </div>'
     );
-
+    new Audio('./mozart-lacrimosa.mp3').play();
 }
